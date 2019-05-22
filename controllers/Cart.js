73cb248e162f9a14875c22bde5446cbe3f74ce7e
Lorenzo <hyperl@hotmail.it>
2019-05-22 18:52:20 +0200

    updated cart service rest

--- controllers/Cart.js
+++ controllers/Cart.js
@@ -17,3 +17,18 @@ module.exports.cartGET = function cartGET (req, res, next) {
       utils.writeJson(res, response);
     });
 };
+
+
+module.exports.cartAddBook = function cartAddBook (req, res, next) {
+  let cartId = req.session[cookie.uid];
+  if(cartId === undefined) {
+    cartId = 0 //with 0 get cart will return an empty json
+  }
+  Cart.cartAddBook(userId, cartId)
+    .then(function (response) {
+      utils.writeJson(res, response);
+    })
+    .catch(function (response) {
+      utils.writeJson(res, response);
+    });
+};
--- service/CartService.js
+++ service/CartService.js
@@ -21,38 +21,23 @@ exports.cartsDbSetup = function(database) {
  * cartId Long 
  * returns Cart
  **/
-exports.cartGET = function(user_id) {
-  //TODO WHEN cookies are in!
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "total" : {
-    "currency" : "eur",
-    "value" : 101.17
-  },
-  "books" : [ {
-    "id" : 0,
-    "title" : "Il deserto dei tartari",
-    "author" : "Dino Buzzati",
-    "price" : {
-      "value" : 10,
-      "currency" : "eur"
-    }
-  }, {
-    "id" : 0,
-    "title" : "Il deserto dei tartari",
-    "author" : "Dino Buzzati",
-    "price" : {
-      "value" : 10,
-      "currency" : "eur"
-    }
-  } ]
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
-    }
-  });
+exports.cartGET = function(userId) {
+  return db.select()
+  .from('carts')
+  .where('user_id', userId)
+}
+
+exports.cartAddBook = function(userId, bookId) {
+  userId = 1;
+  let content = db().select('content').from('carts')
+  .where('user_id', userId)
+  console.log("content taken from id " + userId)
+  console.log(content)
+  if (content[bookId] === undefined) {
+    content[bookId] = 0;
+  }
+  content[bookId] = content[bookId] + 1;
+  console.log("content updated:")
+  console.log(content)
 }
 
