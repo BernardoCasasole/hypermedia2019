9b8780817fd0625649a58d8e4e8203557a5113ea
Bernardo.Casasole <bernardo.casasole@mail.polimi.it>
2019-05-23 18:20:01 +0200

    header and footer common code insertion from a single .js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: { user: 'postgres', password: 'OrnaGile7', database: 'book_db' },
+    connection: { user: 'postgres', database: 'booky_db' },
     migrations: {
       directory: __dirname + '/db/migrations',
     },
---
+++ public/js/header_footer.js
@@ -0,0 +1,345 @@
+document.getElementById("DESKTOP_HEADER").innerHTML =  
+
+			'<div class="wrap_header">'+
+				'<!-- Logo -->'+
+				'<a href="index.html" class="logo">'+
+					'<img src="images/icons/logo.png" alt="IMG-LOGO" dim=80>'+
+				'</a>'+
+
+				'<!-- Menu -->'+
+				'<div class="wrap_menu">'+
+					'<nav class="menu">'+
+						'<ul class="main_menu">'+
+							'<li>'+
+								'<a href="index.html">'+'Home'+'</a>'+
+							'</li>'+
+
+							'<li>'+
+								'<a href="books.html">'+'Books'+'</a>'+
+							'</li>'+
+
+							'<li>'+
+								'<a href="authors.html">'+'Authors'+'</a>'+
+							'</li>'+
+
+							'<li>'+
+								'<a href="events.html">'+'Events'+'</a>'+
+                            '</li>'+
+                            
+							'<li>'+
+								'<a href="contact.html" >'+'Contacts'+'</a>'+ '<!--href="contact.html"-->'+
+							'</li>'+
+						'</ul>'+
+					'</nav>'+
+				'</div>'+
+
+				'<!-- Header Icon -->'+
+				'<div class="header-icons" >'+
+
+						'<div class="header-wrapicon2">'+
+							'<img src="images/icons/icon-magnifier.png" class="header-icon1 js-show-header-dropdown" alt="ICON">'+
+
+							'<!-- Header cart noti -->'+
+							'<div class="header-cart header-dropdown">'+
+								
+								'<ul class="header-cart-wrapitem">'+
+									'<div class="search-product pos-relative bo11 of-hidden">'+
+										'<input class="s-text7 size16 p-l-23 p-r-50" type="text" name="search-product" placeholder="Search Products...">'+
+										'<button class="flex-c-m size5 ab-r-m color2 color0-hov trans-0-4">'+
+											'<i class="fs-12 fa fa-search" aria-hidden="true">'+'</i>'+
+										'</button>'+										
+									'<div class="flex-w">'+
+										'<div class="rs2-select2 bo4 of-hidden w-size12 m-t-5 m-b-5 m-r-10">'+
+											'<select class="selection-2" name="sorting">'+
+												'<option>'+'Default search'+'</option>'+
+												'<option>'+'Search in authors'+'</option>'+
+												'<option>'+'Search in events'+'</option>'+
+												'<option>'+'Search in books'+'</option>'+
+											'</select>'+
+										'</div>'+
+									'</div>'+						
+								'</ul>'+
+							'</div>'+						
+						'</div>'+
+
+					'<span class="linedivide1">'+'</span>'+
+
+					'<a href="#" class="header-wrapicon1 dis-block">'+
+						'<img src="images/icons/icon-header-01.png" class="header-icon1" alt="ICON">'+
+					'</a>'+
+
+					'<span class="linedivide1">'+'</span>'+
+
+					'<div class="header-wrapicon2">'+
+						'<img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">'+
+						'<span class="header-icons-noti">'+'0'+'</span>'+
+
+						'<!-- Header cart noti -->'+
+						'<div class="header-cart header-dropdown">'+
+							'<ul class="header-cart-wrapitem">'+
+								'<li class="header-cart-item">'+
+									'<div class="header-cart-item-img">'+
+										'<img src="images/cart/item-2.jpg" alt="IMG">'+
+									'</div>'+
+
+									'<div class="header-cart-item-txt">'+
+										'<a href="#" class="header-cart-item-name">'+
+											'The Old Man and the Sea'+
+										'</a>'+
+
+										'<span class="header-cart-item-info">'+
+											'1 x $19.00'+
+										'</span>'+
+									'</div>'+
+								'</li>'+
+
+								'<li class="header-cart-item">'+
+									'<div class="header-cart-item-img">'+
+										'<img src="images/cart/item-1.jpg" alt="IMG">'+
+									'</div>'+
+
+									'<div class="header-cart-item-txt">'+
+										'<a href="#" class="header-cart-item-name">'+
+											'Becoming'+
+										'</a>'+
+
+										'<span class="header-cart-item-info">'+
+											'1 x $39.00'+
+										'</span>'+
+									'</div>'+
+								'</li>'+
+
+								'<li class="header-cart-item">'+
+									'<div class="header-cart-item-img">'+
+										'<img src="images/cart/item-6.jpg" alt="IMG">'+
+									'</div>'+
+
+									'<div class="header-cart-item-txt">'+
+										'<a href="#" class="header-cart-item-name">'+
+											'Sostiene Pereira'+
+										'</a>'+
+
+										'<span class="header-cart-item-info">'+
+											'1 x $17.00'+
+										'</span>'+
+									'</div>'+
+								'</li>'+
+							'</ul>'+
+
+							'<div class="header-cart-total">'+
+								'Total: $75.00'+
+							'</div>'+
+
+							'<div class="header-cart-buttons">'+
+								'<div class="header-cart-wrapbtn">'+
+									'<!-- Button -->'+
+									'<a href="cart.html" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">'+
+										'View Cart'+
+									'</a>'+
+								'</div>'+
+
+								'<div class="header-cart-wrapbtn">'+
+									'<!-- Button -->'+
+									'<a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">'+
+										'Check Out'+
+									'</a>'+
+								'</div>'+
+							'</div>'+
+						'</div>'+
+
+					'</div>'+
+				'</div>'+
+			'</div>';
+
+document.getElementById("MOBILE_HEADER").innerHTML = 
+'<div class="wrap_header_mobile">'+
+	'<a href="index.html" class="logo-mobile">'+
+		'<img src="images/icons/logo.png" alt="IMG-LOGO">'+
+	'</a>'+
+
+	'<!-- Button show menu -->'+
+	'<div class="btn-show-menu">'+
+		'<!-- Header Icon mobile -->'+
+		'<div class="header-icons-mobile">'+
+			'<a href="#" class="header-wrapicon1 dis-block">'+
+				'<img src="images/icons/icon-header-01.png" class="header-icon1" alt="ICON">'+
+			'</a>'+
+
+			'<span class="linedivide2">'+'</span>'+
+
+			'<div class="header-wrapicon2">'+
+				'<img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">'+
+				'<span class="header-icons-noti">'+'0'+'</span>'+
+
+				'<!-- Header cart noti -->'+
+				'<div class="header-cart header-dropdown">'+
+					'<ul class="header-cart-wrapitem">'+
+						'<li class="header-cart-item">'+
+							'<div class="header-cart-item-img">'+
+								'<img src="images/cart/item-2.jpg" alt="IMG">'+
+							'</div>'+
+
+							'<div class="header-cart-item-txt">'+
+								'<a href="#" class="header-cart-item-name">'+
+									'The Old Man and the Sea'+
+								'</a>'+
+
+								'<span class="header-cart-item-info">'+
+									'1 x $19.00'+
+								'</span>'+
+							'</div>'+
+						'</li>'+
+
+						'<li class="header-cart-item">'+
+							'<div class="header-cart-item-img">'+
+								'<img src="images/cart/item-1.jpg" alt="IMG">'+
+							'</div>'+
+
+							'<div class="header-cart-item-txt">'+
+								'<a href="#" class="header-cart-item-name">'+
+									'Becoming'+
+								'</a>'+
+
+								'<span class="header-cart-item-info">'+
+									'1 x $39.00'+
+								'</span>'+
+							'</div>'+
+						'</li>'+
+
+						'<li class="header-cart-item">'+
+							'<div class="header-cart-item-img">'+
+								'<img src="images/cart/item-6.jpg" alt="IMG">'+
+							'</div>'+
+
+							'<div class="header-cart-item-txt">'+
+								'<a href="#" class="header-cart-item-name">'+
+									'Sostiene Pereira'+
+								'</a>'+
+
+								'<span class="header-cart-item-info">'+
+									'1 x $17.00'+
+								'</span>'+
+							'</div>'+
+						'</li>'+
+					'</ul>'+
+
+					'<div class="header-cart-total">'+
+						'Total: $75.00'+
+					'</div>'+
+
+					'<div class="header-cart-buttons">'+
+						'<div class="header-cart-wrapbtn">'+
+							'<!-- Button -->'+
+							'<a href="cart.html" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">'+
+								'View Cart'+
+							'</a>'+
+						'</div>'+
+
+						'<div class="header-cart-wrapbtn">'+
+							'<!-- Button -->'+
+							'<a href="#" class="flex-c-m size1 bg1 bo-rad-20 hov1 s-text1 trans-0-4">'+
+								'Check Out'+
+							'</a>'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+			'</div>'+
+		'</div>'+
+
+		'<div class="btn-show-menu-mobile hamburger hamburger--squeeze">'+
+			'<span class="hamburger-box">'+
+				'<span class="hamburger-inner">'+'</span>'+
+			'</span>'+
+		'</div>'+
+	'</div>'+
+'</div>'+
+
+'<div class="wrap-side-menu" >'+
+	'<nav class="side-menu">'+
+		'<ul class="main-menu">'+
+
+
+			'<li class="item-menu-mobile">'+
+				'<a href="index.html">'+'Home'+'</a>'+
+			'</li>'+
+
+			'<li class="item-menu-mobile">'+
+				'<a href="books.html">'+'Books'+'</a>'+
+			'</li>'+
+
+			'<li class="item-menu-mobile">'+
+				'<a href="authors.html">'+'Authors'+'</a>'+
+			'</li>'+
+
+			'<li class="item-menu-mobile">'+
+				'<a href="events.html">'+'Events'+'</a>'+
+			'</li>'+
+			'<li class="item-menu-mobile">'+
+				'<a href="contact.html" >'+'Contacts'+'</a>'+ '<!-- href="contact.html" -->'+
+			'</li>'+
+		'</ul>'+
+	'</nav>'+
+'</div>';
+
+document.getElementById("FOOTER").innerHTML = '<div class="flex-w p-b-90">'+
+'<div class="w-size6 p-t-30 p-l-15 p-r-15 respon4">'+
+	'<h4 class="s-text12 p-b-30">'+
+		'GET IN TOUCH'+
+	'</h4>'+
+
+	'<div>'+
+		'<p class="s-text7 w-size27">'+
+			'<a href="contact.html">'+'Contacts'+'</a>'+
+		'</p>'+
+	'</div>'+
+'</div>'+
+
+'<div class="w-size6 p-t-30 p-l-15 p-r-15 respon4">'+
+	'<h4 class="s-text12 p-b-30">'+
+		'Categories'+
+	'</h4>'+
+
+	'<ul>'+
+		'<li class="p-b-9">'+
+			'<a href="#" class="s-text7">'+
+				'<a href="books.html">'+'Books'+'</a>'+
+			'</a>'+
+		'</li>'+
+
+		'<li class="p-b-9">'+
+			'<a href="#" class="s-text7">'+
+				'<a href="authors.html">'+'Authors'+'</a>'+
+			'</a>'+
+		'</li>'+
+
+		'<li class="p-b-9">'+
+			'<a href="#" class="s-text7">'+
+				'<a href="events.html">'+'Events'+'</a>'+
+			'</a>'+
+		'</li>'+
+
+	'</ul>'+
+'</div>'+
+
+'<div class="w-size6 p-t-30 p-l-15 p-r-15 respon4">'+
+	'<h4 class="s-text12 p-b-30">'+
+		'LINKS'+
+	'</h4>'+
+	'<ul>'+
+		'<li class="p-b-9">'+
+			'<a href="#" class="s-text7">'+
+				'<a href="cart.html">'+'Cart'+'</a>'+
+			'</a>'+
+		'</li>'+
+	'</ul>'+
+'</div>'+
+
+
+'</div>'+
+
+'<div class="t-center p-l-15 p-r-15">'+
+
+'<div class="t-center s-text8 p-t-20">'+
+	'Copyright © 2019 All rights reserved.'+
+'</div>'+
+'</div>';
